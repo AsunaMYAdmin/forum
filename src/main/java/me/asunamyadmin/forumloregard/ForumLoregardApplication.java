b6dc73473c08eae3b719daf59a41abea083131e5
AsunaMYAdmin <kiritoadm@mail.ru>
2026-03-15 16:12:29 +0700

    Init commit

---
+++ src/main/java/me/asunamyadmin/forumloregard/ForumLoregardApplication.java
@@ -0,0 +1,13 @@
+package me.asunamyadmin.forumloregard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumLoregardApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ForumLoregardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/asunamyadmin/forumloregard/ForumLoregardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.asunamyadmin.forumloregard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumLoregardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
